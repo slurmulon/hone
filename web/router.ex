9cc817fef06fbec564b18a11faa472ecf902aa62
evavro <me@madhax.io>
2016-12-17 21:18:11 -0800

    :wrench: added version to routes

--- web/router.ex
+++ web/router.ex
@@ -23,12 +23,12 @@ defmodule Hone.Router do
   scope "/api", Hone do
     pipe_through :api
 
-    resources "/boards", BoardController, except: [:new, :edit]
-    resources "/projects", ProjectController, except: [:new, :edit]
-    resources "/stages", StageController, except: [:new, :edit]
-    resources "/units", UnitController, except: [:new, :edit]
-    resources "/notes", NoteController, except: [:new, :edit]
-    resources "/tags", TagController, except: [:new, :edit]
-    resources "/users", UserController, except: [:new, :edit]
+    resources "/v1/boards", BoardController, except: [:new, :edit]
+    resources "/v1/projects", ProjectController, except: [:new, :edit]
+    resources "/v1/stages", StageController, except: [:new, :edit]
+    resources "/v1/units", UnitController, except: [:new, :edit]
+    resources "/v1/notes", NoteController, except: [:new, :edit]
+    resources "/v1/tags", TagController, except: [:new, :edit]
+    resources "/v1/users", UserController, except: [:new, :edit]
   end
 end
