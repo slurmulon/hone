7c694a9a9483442d3271daa4fe105d4295f2f138
evavro <me@madhax.io>
2017-03-12 15:51:56 -0700

    :tv: :truck: migrated portal component to stylus

--- ui/src/views/Portal.vue
+++ ui/src/views/Portal.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="main" class="portal">
     <div class="page">
-      <!-- TODO: break this out into portal-browse or the like -->
+      <!-- TODO break this out into portal-browse or the like -->
       <div class="actions pure-form">
         <!-- <div class="action group dashboards">
           <a href="#">
@@ -46,7 +46,7 @@
         </div>
       </div>
 
-      <!-- TODO: make these generic so that people can just add
+      <!-- TODO make these generic so that people can just add
       arbitrary boxes and specify a title -->
       <section>
         <div class="pinned panel pure-g">
@@ -114,230 +114,190 @@ export default {
 }
 </script>
 
-<style scoped>
-#main {
-  margin-left: 150px; /* TODO: remove need for this once flexbox stuff is done; */
-  /*padding-left: 2em;
-  padding-top: 1em;*/
-  /* background: #fff; */
+<style lang="stylus" scoped>
+#main
+  margin-left 150px /* TODO remove need for this once flexbox stuff is done */
+  //padding-left 2em
+  //padding-top 1em
+  //background #fff
 
-  /*flex: 1 100%;*/ /* TODO: use this once layout is fixed a bit; */
-}
+  /*flex 1 100% /* TODO use this once layout is fixed a bit */
 
-#layout {
-  display: flex;
-  align-items: stretch;
-  background: #261E25;
-  height: 100%;
-}
+#layout
+  display flex
+  align-items stretch
+  background #261E25
+  height 100%
 
-#layout header {
-  flex: 0 10%;
-}
+#layout header
+  flex 0 10%
 
-.portal .pure-g {
-  letter-spacing: 0;
-}
+.portal .pure-g
+  letter-spacing 0
 
-.portal h1 {
-  font-weight: normal;
-}
 
-.portal h2 {
-  margin: 0;
-}
+.portal h1
+  font-weight normal
 
-.portal .page {
-  display: flex;
-  flex-flow: row wrap;
-  flex: 1 90%;
-  align-content: flex-start;
-  /*overflow-y: scroll;*/
-}
+.portal h2
+  margin 0
 
-.portal .page > .actions {
-  display: inline-flex;
-  flex-flow: row nowrap;
-  flex: 1 100%;
-  line-height: 5em;
-  height: 5em;
-  border-bottom: 1px solid #333;
-}
+.portal .page
+  display flex
+  flex-flow row wrap
+  flex 1 90%
+  align-content flex-start
+  /*overflow-y scroll*/
 
-.portal .page > .actions a {
-  display: inline-block;
-  width: 100%;
-  text-align: center;
-  text-decoration: none;
-  white-space: nowrap;
-  /*color: #809c9e;*/
-  /*color: #baced3;*/
-  color: #a1b1b5;
-  /*color: #aaa;*/
-  /*color: #525252;*/
-  border-right: 1px solid #333; /*#5a839b;*/
-  padding-left: 1em;
-}
+  & > .actions
+    display inline-flex
+    flex-flow row nowrap
+    flex 1 100%
+    line-height 5em
+    height 5em
+    border-bottom 1px solid #333
 
-.portal .page > .actions a:hover {
-  color: #f5f5f5;
-  background: #261E21;
-  border-bottom: 2px solid #a4c8dd;
-}
+    a
+      display inline-block
+      width 100%
+      text-align center
+      text-decoration none
+      white-space nowrap
+      /*color #809c9e*/
+      /*color #baced3*/
+      color #a1b1b5
+      /*color #aaa*/
+      /*color #525252*/
+      border-right 1px solid #333 /*#5a839b*/
+      padding-left 1em
 
-.portal .page > .actions i {
-  padding-right: .5em;
-}
+      &:hover
+        color #f5f5f5
+        background #261E21
+        border-bottom 2px solid #a4c8dd
 
-.portal .page > .actions input {
-  width: 100%;
-  border-top: none;
-}
+    i
+      padding-right .5em
 
-.portal .page > .actions .group {
-  flex: 1;
-}
+    input
+      width 100%
+      border-top none
 
-.portal .page > .actions .group i {
-  padding-left: .5em;
-}
+    .group
+      flex 1
 
-.portal .page > .actions .search-input {
-  flex: 3;
-}
+      i
+        padding-left .5em
 
-.portal .page > .actions .search-input > input {
-  padding: 0 1.5em;
-}
+    .search-input
+      flex 3
+
+      & > input
+        padding 0 1.5em
+        
+    .item, .group, input, select
+      height 5em
+      
+  section
+    /*display flex
+    align-content stretch
+    justify-content space-around*/
+    /*align-items stretch
+    justify-content space-between*/
+
+    /*display flex*/
+    display inline-flex
+    flex-flow row wrap
+    justify-content space-around
+    align-content stretch
+    flex 1 100%
+    /*flex 0 90%*/
+    /*width 90%*/
+    /*padding-left 2em
+    padding-top 1em*/
 
 .portal .search-input,
-.portal .search-go {
-  text-align: center;
-}
+.portal .search-go
+  text-align center
 
-.portal .page > .actions .item, .group, input, select {
-  height: 5em;
-}
+.portal .panel
+  position relative
+  width 100%
+  padding 2em
+  /*border 1px solid #686884*/
 
-.portal .page > section {
-  /*display: flex;
-  align-content: stretch;
-  justify-content: space-around;*/
-  /*align-items: stretch;
-  justify-content: space-between;*/
+  .controls
+    position absolute
+    right 2em
 
-  /*display: flex;*/
-  display: inline-flex;
-  flex-flow: row wrap;
-  justify-content: space-around;
-  align-content: stretch;
-  flex: 1 100%;
-  /*flex: 0 90%;*/
-  /*width: 90%;*/
-  /*padding-left: 2em;
-  padding-top: 1em;*/
-}
+    a
+      /*color pink*/ /* actually looks good */
+      color #a4c8dd
+      opacity 0.25
+      margin-right 1em
+      
+      &:hover
+        opacity 0.75
+        
+  & > .list
+    width 100%
+    margin-top 1em
 
-.portal .panel {
-  position: relative;
-  width: 100%;
-  padding: 2em;
-  /*border: 1px solid #686884;*/
-}
+    .item
+      display inline-flex
+      justify-content space-between
+      justify-content flex-start
+      width 100%
+      padding 1em
+      text-decoration none
+      color #777
+      border-bottom 1px solid #333
+      
+      &:hover
+        background #2d272d
+        
+      & > .title
+        flex 2
 
-.portal .panel .controls {
-  position: absolute;
-  right: 2em;
-}
+      i
+        padding-right 1em
+        flex 5% 0
+        
+        &:last-of-type
+          text-align right
 
-.portal .panel .controls a {
-  /*color: pink;*/ /* actually looks good */
-  color: #a4c8dd;
-  opacity: 0.25;
-  margin-right: 1em;
-}
+  &.pinned
+    flex 100%
 
-.portal .panel .controls a:hover {
-  opacity: 0.75;
-}
+  &.assigned, &.log
+    flex 2
 
-.portal .panel > .list {
-  width: 100%;
-  margin-top: 1em;
-}
+/* FIXME better solution is to surround the section and feed with a parent flex container */
+.portal .feed
+  /*flex 1 10%*/
+  flex 1 12%
+  /*border-left 1px solid #456272*/
+  border-left 1px solid #686884
+  background #2b222a
+  
+  &.empty
+    height calc(100% - 100px)
+    display flex
+    justify-content center
+    align-items center
+    margin 0 auto
+    padding 1em
+    border-left none
+    text-align center
+    opacity 0.25
 
-.portal .panel > .list .item {
-  display: inline-flex;
-  justify-content: space-between;
-  justify-content: flex-start;
-  width: 100%;
-  padding: 1em;
-  text-decoration: none;
-  color: #777;
-  border-bottom: 1px solid #333;
-}
-
-.portal .panel > .list .item:hover {
-  background: #2d272d;
-}
-
-.portal .panel > .list .item i {
-  padding-right: 1em;
-  flex: 5% 0;
-}
-
-.portal .panel > .list .item i:last-of-type {
-  text-align: right;
-}
-
-.portal .panel > .list .item > .title {
-  flex: 2;
-}
-
-.portal .panel.pinned {
-  flex: 100%;
-}
-
-.portal .panel.assigned,
-.portal .panel.log {
-  flex: 2;
-}
-
-/* FIXME: better solution is to surround the section and feed with a parent flex container */
-.portal .feed {
-  /*position: fixed;
-  top: 0;
-  right: 0;
-  bottom: 0;
-  width: 10%;*/
-
-  /*flex: 1 10%;*/
-  flex: 1 12%;
-  /*border-left: 1px solid #456272;*/
-  border-left: 1px solid #686884;
-  background: #2b222a;
-}
-
-.portal .feed.empty {
-  height: calc(100% - 100px);
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  margin: 0 auto;
-  padding: 1em;
-  border-left: none;
-  text-align: center;
-  opacity: 0.25;
-}
-
-.portal .feed h2 {
-  /*color: #a4c8dd;*/
-  color: #686884;
-  width: 100%;
-  padding: 1em 0;
-  height: 100px;
-  text-align: center;
-  border-bottom: 1px solid #333;
-}
+  h2
+    /*color #a4c8dd*/
+    color #686884
+    width 100%
+    padding 1em 0
+    height 100px
+    text-align center
+    border-bottom 1px solid #333
 </style>
 
